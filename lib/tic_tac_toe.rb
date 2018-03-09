b0dd0fb061d3d8bf40d5a7b7356138448da0245d
Karen Flores <karen.ag.flores@gmail.com>
2018-03-09 06:17:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,8 +126,8 @@ end
 def play(board)
   while over?(board) == false
     turn(board)
-    #won?(board)
-    #draw?(board)
+    won?(board)
+    draw?(board)
   end
   if won?(board)
     puts "Congratulations #{winner(board)}!"
