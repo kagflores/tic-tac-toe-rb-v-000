665709123b91a5f14cde1140861d856c3cd009b8
Karen Flores <karen.ag.flores@gmail.com>
2018-03-09 06:16:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,6 @@ def turn (board)
   user_choice = gets.strip
   index = input_to_index(user_choice)
   if valid_move?(board, index)
-    #current_player(board)
     move(board, index, current_player(board))
     display_board(board)
   else
